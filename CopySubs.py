63eb32650b0ced3865d3cc75624bdc4c0caad12d
tvvoty <neketa777@gmail.com>
2021-09-14 09:30:36 +0300

    Subtitle seeking actually works now

--- CopySubs.py
+++ CopySubs.py
@@ -8,11 +8,11 @@ def convert_sub_time_to_miliseconds(hours, minutes, seconds, miliseconds):
     # ms_from_seconds = seconds * 1000
 
     # return miliseconds + ms_from_hours + ms_from_minutes + ms_from_seconds
-    return miliseconds + (seconds * 1000) + (minutes * 60000) + (hours * 3600000)
+    return int(miliseconds + "0") + (int(seconds) * 1000) + (int(minutes) * 60000) + (int(hours) * 3600000)
 
 
 def convert_sub_timestamp_to_miliseconds(seconds, miliseconds):
-    return miliseconds + (seconds * 1000)
+    return int(miliseconds[0:3]) + (int(seconds) * 1000)
 
 
 # Patterns section
@@ -25,11 +25,11 @@ subtitle_pattern = re.compile(
 
 # File section
 subtititle_file = "/home/tvvoty/LinuxAdd/Vlcfilms/9/Barakamon09.ass"  # 508 dialogs in this file
-log_file = "/home/tvvoty/LinuxAdd/GitHub/Copy_SMPlayer_Subs/SMPlayerLogs.log"
-# log_text = pyperclip.paste()
+# log_file = "/home/tvvoty/LinuxAdd/GitHub/Copy_SMPlayer_Subs/SMPlayerLogs.log"
+log_text = pyperclip.paste()
 
-with open(log_file, mode='r', encoding='utf-8') as file:
-    log_text = file.read()
+# with open(log_file, mode='r', encoding='utf-8') as file:
+#     log_text = file.read()
 
 with open(subtititle_file, mode='r', encoding='utf-8') as file:
     subtititle_file_content = file.read()
@@ -61,8 +61,8 @@ for x in dialogues_list:
 
 start_sub_hour = subtitle_pattern.findall(subtititle_file_content)
 
-print(
-    f"The captured time stamp seconds is: {time_stamps_findall[-1][0]} \n The captured time stamp ms is: {time_stamps_findall[-1][1]}")
+# print(
+#     f"The captured time stamp seconds is: {time_stamps_findall[-1][0]} \n The captured time stamp ms is: {time_stamps_findall[-1][1]}")
 
 time_stamp_in_ms = convert_sub_timestamp_to_miliseconds(
     time_stamps_findall[-1][0], time_stamps_findall[-1][1])
@@ -71,10 +71,10 @@ subtitle = []
 for sub_line in subtitle_pattern.findall(subtititle_file_content):
     sub_line_start = convert_sub_time_to_miliseconds(
         sub_line[0], sub_line[1], sub_line[2], sub_line[3])
-    print(f"sub_line_start = {sub_line_start}")
+    # print(f"sub_line_start = {sub_line_start}")
     sub_line_end = convert_sub_time_to_miliseconds(
         sub_line[4], sub_line[5], sub_line[6], sub_line[7])
-    print(f"sub_line_end = {sub_line_end}")
+    # print(f"sub_line_end = {sub_line_end}")
     if sub_line_start < time_stamp_in_ms < sub_line_end:
         subtitle.append(sub_line)
 
@@ -85,6 +85,6 @@ print(f"The matching subtitle is: {subtitle}")
 # print(match.group(1)[-1])
 # print(f"The last time stamp is: {time_stamps_list[-1]}")
 
-# pyperclip.copy("subtitles")
+pyperclip.copy(subtitle[0][8])
 
 # log_text = pyperclip.paste()
--- try.py
+++ try.py
@@ -1,9 +1,26 @@
 import pyperclip
 import re
 
-
-def convert_sub_timestamp_to_miliseconds(seconds, miliseconds):
-    return miliseconds + (seconds * 1000)
+#
 
 
-print(convert_sub_timestamp_to_miliseconds(8633, 024000))
+# def convert_sub_time_to_miliseconds(hours, minutes, seconds, miliseconds):
+#     # ms_from_hours = hours * 3600000
+#     # ms_from_minutes = minutes * 60000
+#     # ms_from_seconds = seconds * 1000
+#
+#     # return miliseconds + ms_from_hours + ms_from_minutes + ms_from_seconds
+#     return int(miliseconds) + (int(seconds) * 1000) + (int(minutes) * 60000) + (int(hours) * 3600000)
+#
+#
+# def convert_sub_timestamp_to_miliseconds(seconds, miliseconds):
+#     return int(miliseconds) + (int(seconds) * 1000)
+#
+#
+# if 2 < 3 < 4:
+#     print("Yes")
+
+
+x = 60000 + 62000
+print(x)
+print(x / 2)
